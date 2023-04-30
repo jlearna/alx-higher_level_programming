d59f74082a87c90ae3a90b779f3559e73328d679
James Aworo <james001.awo@gmail.com>
2023-04-30 04:39:28 -0700

    fix task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""A python script thats Displays the X-Request-Id header variable of a request to a given URL
 """
-import sys
+Given URL as parameter, fetch URL and display value from reponse header
+usage: ./5-hbtn_header https://intranet.hbtn.io
+"""
+from sys import argv
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
