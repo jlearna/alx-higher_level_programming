bd788e15d660b79fc5e61009e934a60397fbfda4
James Aworo <james001.awo@gmail.com>
2023-04-30 06:48:47 -0700

    update task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a POST request to 0.0.0.0:5000/catch_me with a custom header
-curl -s -XPOST -d 'msg=test' '0.0.0.0:5000/catch_me' -o /dev/null
+curl -sL -X PUT -H "Origin: Alx" -d "task_id=102" 0.0.0.0:5000/catch_me
