ace3f96f5884af41f71c86612276a3cc3e0e8ebd
James Aworo <james001.awo@gmail.com>
2023-04-30 06:54:08 -0700

    fix 102 checker error

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Make a POST request to 0.0.0.0:5000/catch_me with a custom header
-curl -sL -X PUT -H "Origin: Alx" -d "task_id=102" 0.0.0.0:5000/catch_me
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
