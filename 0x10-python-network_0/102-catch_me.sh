5bfa65abd17ad98dc3c330d22aaeea4da2db62a3
James Aworo <james001.awo@gmail.com>
2023-04-30 06:56:49 -0700

    update task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Make a POST request to 0.0.0.0:5000/catch_me with a custom header
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with: You got me!, in the body of the response
 curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
